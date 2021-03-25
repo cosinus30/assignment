21e0f30da7a8d3840cc64c7dc7386e72adec25bb
Kerem <krmtrk20@gmail.com>
2021-03-25 19:45:11 +0300

    add landing page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
     <v-app>
-        <v-app-bar app color="primary" dark>
+        <v-app-bar app dark>
             <div class="d-flex align-center">
                 <v-img
-                    alt="Vuetify Logo"
+                    alt="Pitcher Logo"
                     class="shrink mr-2"
                     contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                    src="https://www.pitcher.com/hubfs/identity/logos/anniversary-logo/210114_pitcher_logo_anniversary_white_RGB.svg"
                     transition="scale-transition"
                     width="40"
-                />
-
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
                 />
             </div>
 
@@ -25,7 +16,7 @@
                 <input
                     v-model="search"
                     type="text"
-                    placeholder="Type..."
+                    placeholder="Search..."
                     class=" red lighten-5 rounded-pill px-4 py-2"
                 />
                 <v-icon style="position: absolute; right: 8px; top: 8px; color: black;">
@@ -35,7 +26,18 @@
         </v-app-bar>
 
         <v-main>
-            <HelloWorld :searchTerm="finalSearchTerm"/>
+            <HelloWorld v-if="touched" :searchTerm="finalSearchTerm" />
+            <template v-else>
+                    <div style="position: relative; height:1080px;">
+                        <v-img
+                            src="https://cdn.dribbble.com/users/2187949/screenshots/7780753/media/185ccbcc0d1fbec028e867136e5096a3.jpg"
+                        >
+                        </v-img>
+                        <h1 style="position: absolute; top: 128px; left: 128px;">
+                            Why don't you start typing already?
+                        </h1>
+                    </div>
+            </template>
         </v-main>
     </v-app>
 </template>
@@ -52,17 +54,21 @@ export default {
 
     data: () => ({
         search: "",
+        touched: false,
         finalSearchTerm: "",
-        timer: {}
+        timer: {},
     }),
 
     watch: {
-      search: function() {
-          clearTimeout(this.timer);
-          this.timer = setTimeout(() => {
-            this.finalSearchTerm = this.search;
-          }, 1000)
-      },
+        search: function() {
+            clearTimeout(this.timer);
+            this.timer = setTimeout(() => {
+                this.finalSearchTerm = this.search;
+            }, 1000);
+
+            if (this.search === "") this.touched = false;
+            else this.touched = true;
+        },
     },
 };
 </script>
