7d3623604bdd48c1e5ac1f60d4ba7f8fa0d4a7ac
Kerem <krmtrk20@gmail.com>
2021-03-25 14:40:19 +0300

    add dummy search bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,57 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <div class="d-flex align-center">
+                <v-img
+                    alt="Vuetify Logo"
+                    class="shrink mr-2"
+                    contain
+                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                    transition="scale-transition"
+                    width="40"
+                />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+                <v-img
+                    alt="Vuetify Name"
+                    class="shrink mt-1 hidden-sm-and-down"
+                    contain
+                    min-width="100"
+                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+                    width="100"
+                />
+            </div>
 
-      <v-spacer></v-spacer>
+            <div style="position: relative;" class="mx-auto">
+                <input
+                    v-model="searchTerm"
+                    type="text"
+                    placeholder="Search..."
+                    class=" red lighten-5 rounded-pill px-4 py-2"
+                />
+                <v-icon style="position: absolute; right: 8px; top: 8px; color: black;">
+                    mdi-magnify
+                </v-icon>
+            </div>
+        </v-app-bar>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+        <v-main>
+            <HelloWorld />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+    name: "App",
 
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld,
+    },
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        searchTerm: "",
+    }),
 };
 </script>
