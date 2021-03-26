83e34d99769fe70c86942a316f9f50c5327eca1f
Kerem <krmtrk20@gmail.com>
2021-03-26 13:29:01 +0300

    add axios
    remove vue-resource since it does not work well with composition

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import VueResource from "vue-resource";
 import VueCompositionApi from '@vue/composition-api'
 
 Vue.config.productionTip = false;
 
-Vue.use(VueResource);
 Vue.use(VueCompositionApi);
 
 var store = {
